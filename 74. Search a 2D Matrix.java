71b2f7f2adf2515df542118c9d494a8b8fee4cd6
Aditya Sharma <78315735+Aditya9675@users.noreply.github.com>
2022-08-11 18:24:43 +0530

    Create 74. Search a 2D Matrix.java

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,57 @@
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+         if(matrix==null || matrix.length==0 || matrix[0].length==0) 
+            return false;
+ 
+        int m = matrix.length;
+        int n = matrix[0].length;
+ 
+        int start = 0;
+        int end = m*n-1;
+ 
+        while(start<=end){
+            int mid=(start+end)/2;
+            int midX=mid/n;
+            int midY=mid%n;
+ 
+            if(matrix[midX][midY]==target) 
+                return true;
+ 
+            if(matrix[midX][midY]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+ 
+        return false;
+    }
+}
